168d0941d322f6b7407adeb23ce802315d9a6f1d
Cole Johnson <john1739@purdue.edu>
2017-01-14 15:53:55 -0500

    Added small caps

--- angularStyle.js
+++ angularStyle.js
@@ -1,6 +1,13 @@
-var angularStyle = angular.module('angularStyle', ['ngRoute']);
+var config = {
+    pixelMultiplier: 4,
+    smallCaps: {
+        size: 11,
+        weight: 700,
+        color: '#8A95A6'
+    }
+};
 
-var MULTI = 4; // Pixel Multiplier
+var angularStyle = angular.module('angularStyle', ['ngRoute']);
 
 /*
  * MARGIN DIRECTIVES
@@ -8,7 +15,7 @@ var MULTI = 4; // Pixel Multiplier
 angularStyle.directive('asMXy', function() {
     return { restrict: 'A', scope: { asMXy: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asMXy + 'px';
+            var pixels = config.pixelMultiplier * scope.asMXy + 'px';
             element.css({ margin: pixels });
         }
     };
@@ -16,7 +23,7 @@ angularStyle.directive('asMXy', function() {
 angularStyle.directive('asMX', function() {
     return { restrict: 'A', scope: { asMX: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asMX + 'px';
+            var pixels = config.pixelMultiplier * scope.asMX + 'px';
             var margin = '0 ' + pixels;
             element.css({ margin: margin, });
         }
@@ -25,7 +32,7 @@ angularStyle.directive('asMX', function() {
 angularStyle.directive('asMY', function() {
     return { restrict: 'A', scope: { asMY: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asMY + 'px';
+            var pixels = config.pixelMultiplier * scope.asMY + 'px';
             var margin = pixels + ' 0';
             element.css({ margin: margin });
         }
@@ -34,7 +41,7 @@ angularStyle.directive('asMY', function() {
 angularStyle.directive('asMT', function() {
     return { restrict: 'A', scope: { asMT: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asMT + 'px';
+            var pixels = config.pixelMultiplier * scope.asMT + 'px';
             var margin = pixels + ' 0 0 0';
             element.css({ margin: margin });
         }
@@ -43,7 +50,7 @@ angularStyle.directive('asMT', function() {
 angularStyle.directive('asMB', function() {
     return { restrict: 'A', scope: { asMB: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asMB + 'px';
+            var pixels = config.pixelMultiplier * scope.asMB + 'px';
             var margin = '0 0 ' + pixels + ' 0';
             element.css({ margin: margin });
         }
@@ -52,7 +59,7 @@ angularStyle.directive('asMB', function() {
 angularStyle.directive('asML', function() {
     return { restrict: 'A', scope: { asML: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asML + 'px';
+            var pixels = config.pixelMultiplier * scope.asML + 'px';
             var margin = '0 0 0 ' + pixels;
             element.css({ margin: margin });
         }
@@ -61,7 +68,7 @@ angularStyle.directive('asML', function() {
 angularStyle.directive('asMR', function() {
     return { restrict: 'A', scope: { asMR: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asMR + 'px';
+            var pixels = config.pixelMultiplier * scope.asMR + 'px';
             var margin = '0 ' + pixels + ' 0 0';
             element.css({ margin: margin });
         }
@@ -73,7 +80,7 @@ angularStyle.directive('asMR', function() {
 angularStyle.directive('asPXy', function() {
     return { restrict: 'A', scope: { asPXy: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asPXy + 'px';
+            var pixels = config.pixelMultiplier * scope.asPXy + 'px';
             element.css({ padding: pixels });
         }
     };
@@ -81,7 +88,7 @@ angularStyle.directive('asPXy', function() {
 angularStyle.directive('asPX', function() {
     return { restrict: 'A', scope: { asPX: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asPX + 'px';
+            var pixels = config.pixelMultiplier * scope.asPX + 'px';
             var padding = '0 ' + pixels;
             element.css({ padding: padding, });
         }
@@ -90,7 +97,7 @@ angularStyle.directive('asPX', function() {
 angularStyle.directive('asPY', function() {
     return { restrict: 'A', scope: { asPY: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asPY + 'px';
+            var pixels = config.pixelMultiplier * scope.asPY + 'px';
             var padding = pixels + ' 0';
             element.css({ padding: padding });
         }
@@ -99,7 +106,7 @@ angularStyle.directive('asPY', function() {
 angularStyle.directive('asPT', function() {
     return { restrict: 'A', scope: { asPT: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asPT + 'px';
+            var pixels = config.pixelMultiplier * scope.asPT + 'px';
             var padding = pixels + ' 0 0 0';
             element.css({ padding: padding });
         }
@@ -108,7 +115,7 @@ angularStyle.directive('asPT', function() {
 angularStyle.directive('asPB', function() {
     return { restrict: 'A', scope: { asPB: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asPB + 'px';
+            var pixels = config.pixelMultiplier * scope.asPB + 'px';
             var padding = '0 0 ' + pixels + ' 0';
             element.css({ padding: padding });
         }
@@ -117,7 +124,7 @@ angularStyle.directive('asPB', function() {
 angularStyle.directive('asPL', function() {
     return { restrict: 'A', scope: { asPL: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asPL + 'px';
+            var pixels = config.pixelMultiplier * scope.asPL + 'px';
             var padding = '0 0 0 ' + pixels;
             element.css({ padding: padding });
         }
@@ -126,9 +133,24 @@ angularStyle.directive('asPL', function() {
 angularStyle.directive('asPR', function() {
     return { restrict: 'A', scope: { asPR: '=' },
         link: function(scope, element, attr) {
-            var pixels = MULTI * scope.asPR + 'px';
+            var pixels = config.pixelMultiplier * scope.asPR + 'px';
             var padding = '0 ' + pixels + ' 0 0';
             element.css({ padding: padding });
+        }
+    };
+});
+/*
+ * FONT DIRECTIVES
+ */
+angularStyle.directive('asFSmallCaps', function() {
+    return { restrict: 'A',
+        link: function(scope, element, attr) {
+            element.css({
+                'font-size': config.smallCaps.size + 'px',
+                'text-transform': 'uppercase',
+                'font-weight': config.smallCaps.weight,
+                'color': config.smallCaps.color
+            });
         }
     };
 });
